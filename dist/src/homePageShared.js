184a378662b2089816fa7dbc695ef22be5466fc7
fenghuo1943 <1169279815@qq.com>
2026-05-21 09:57:05 +0800

    删除dist同步

--- dist/src/homePageShared.js
+++
@@ -1,3 +0,0 @@
-export {};
-// 注意：shortcuts 现在由 settings.ts 中的 defaultShortcuts 定义
-// 此文件保留类型定义供其他模块使用
--- dist/src/main.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from 'vue';
-import HomePage from './HomePage.vue';
-import './styles/global.css';
-createApp(HomePage).mount('#app');
